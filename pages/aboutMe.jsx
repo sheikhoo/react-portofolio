9b9f2f01a7926832e34146473317ed4e39624597
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2023-03-12 07:58:09 +0330

    add info

--- pages/aboutMe.jsx
+++ pages/aboutMe.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Me from '../public/img/me.jpg';
 import Image from 'next/image';
+import info from '../utils/info';
 
 export default function AboutMe() {
   return (
     <div className="text-center p-10">
       <h2 className="text-5xl md:text-6xl py-2 text-teal-600 font-yekan dark:text-red-600">
-        محمد صادق شیخ زاهدی
+        {info.name}
       </h2>
       <div className="relative mx-auto mt-5 w-36 h-36">
         <Image src={Me} className="rounded-full" alt="my image" />
