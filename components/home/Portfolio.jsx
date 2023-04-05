ce51c488ccbe6799c6fdf5dbe640522812f8954b
behroozshafei.official <behroozshafei.official@gmail.com>
2023-04-05 19:04:58 +0330

    fix portfolio responsive for mobile

--- components/home/Portfolio.jsx
+++ components/home/Portfolio.jsx
@@ -10,23 +10,23 @@ export default function Portfolio() {
   return (
     <section>
       <h3 className="text-3xl py-1 font-yekan dark:text-white">پروژه های من</h3>
-      <div className="grid grid-cols-3 gap-10">
-        <section class="container mx-auto p-10 md:py-20 px-0 md:p-20 md:px-0 antialiased">
+      <div className="lg:flex gap-10">
+        <section className="container mx-auto p-10 md:py-20 px-0 md:p-20 md:px-0 antialiased flex-1">
           <section>
             <article
-              class="mx-auto max-w-sm shadow-xl bg-cover bg-center min-h-150 transform duration-500 hover:-translate-y-2 cursor-pointer group"
+              className="mx-auto max-w-sm shadow-xl bg-cover bg-center min-h-150 transform duration-500 hover:-translate-y-2 cursor-pointer group"
               style={{
                 backgroundImage: `url(${Mhesam.src})`,
                 width: "100%",
                 height: "100%",
               }}
             >
-              <div class="bg-black bg-opacity-20 min-h-150 px-10 flex flex-wrap flex-col pt-96 hover:bg-opacity-75 transform duration-300">
-                <h1 class="text-white text-3xl mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300">
+              <div className="bg-black bg-opacity-20 min-h-150 px-10 flex flex-wrap flex-col pt-96 hover:bg-opacity-75 transform duration-300">
+                <h1 className="text-white text-3xl mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300">
                   همیاری اجتماعی
                 </h1>
-                <div class="w-16 h-2 bg-red-500 rounded-full mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300"></div>
-                <p class="opacity-0 text-white text-xl group-hover:opacity-80 transform duration-500 mb-5">
+                <div className="w-16 h-2 bg-red-500 rounded-full mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300"></div>
+                <p className="opacity-0 text-white text-xl group-hover:opacity-80 transform duration-500 mb-5">
                   سامانه همیاری اجتماعی سامانه ای است برای متقاضیان عضویت در
                   نهاد مردمی رسالت که با احراز هویت غیرحضوری روند ثبت نام اعضا
                   را تسهیل بخشیده است.
@@ -35,22 +35,22 @@ export default function Portfolio() {
             </article>
           </section>
         </section>
-        <section class="container mx-auto p-10 md:py-20 px-0 md:p-20 md:px-0 antialiased">
-          <section class=" ">
+        <section className="container mx-auto p-10 md:py-20 px-0 md:p-20 md:px-0 antialiased flex-1">
+          <section className=" ">
             <article
-              class="mx-auto max-w-sm shadow-xl bg-cover bg-center min-h-150 transform duration-500 hover:-translate-y-2 cursor-pointer group"
+              className="mx-auto max-w-sm shadow-xl bg-cover bg-center min-h-150 transform duration-500 hover:-translate-y-2 cursor-pointer group"
               style={{
                 backgroundImage: `url(${Mbazar.src})`,
                 width: "100%",
                 height: "100%",
               }}
             >
-              <div class="bg-black bg-opacity-20 min-h-150 px-10 flex flex-wrap flex-col pt-96 hover:bg-opacity-75 transform duration-300">
-                <h1 class="text-white text-3xl mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300">
+              <div className="bg-black bg-opacity-20 min-h-150 px-10 flex flex-wrap flex-col pt-96 hover:bg-opacity-75 transform duration-300">
+                <h1 className="text-white text-3xl mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300">
                   ام بازار
                 </h1>
-                <div class="w-16 h-2 bg-red-500 rounded-full mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300"></div>
-                <p class="opacity-0 text-white text-xl group-hover:opacity-80 transform duration-500 mb-5">
+                <div className="w-16 h-2 bg-red-500 rounded-full mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300"></div>
+                <p className="opacity-0 text-white text-xl group-hover:opacity-80 transform duration-500 mb-5">
                   سامانه ای برای عرضه و تقاضا منصفانه و اعتمادساز برای اعضا و
                   هسته های کارآفرینی اجتماعی و زمینه سازی برای تامین کالا جهت
                   فعالیت های تولیدی و خدماتی هسته های کارآفرینی اجتماعی
@@ -59,22 +59,22 @@ export default function Portfolio() {
             </article>
           </section>
         </section>
-        <section class="container mx-auto p-10 md:py-20 px-0 md:p-20 md:px-0 antialiased">
-          <section class=" ">
+        <section className="container mx-auto p-10 md:py-20 px-0 md:p-20 md:px-0 antialiased flex-1">
+          <section className=" ">
             <article
-              class="mx-auto max-w-sm shadow-xl bg-cover bg-center min-h-150 transform duration-500 hover:-translate-y-2 cursor-pointer group"
+              className="mx-auto max-w-sm shadow-xl bg-cover bg-center min-h-150 transform duration-500 hover:-translate-y-2 cursor-pointer group"
               style={{
                 backgroundImage: `url(${Mresalat.src})`,
                 width: "100%",
                 height: "100%",
               }}
             >
-              <div class="bg-black bg-opacity-20 min-h-150 px-10 flex flex-wrap flex-col pt-96 hover:bg-opacity-75 transform duration-300">
-                <h1 class="text-white text-3xl mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300">
+              <div className="bg-black bg-opacity-20 min-h-150 px-10 flex flex-wrap flex-col pt-96 hover:bg-opacity-75 transform duration-300">
+                <h1 className="text-white text-3xl mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300">
                   ام حسام
                 </h1>
-                <div class="w-16 h-2 bg-red-500 rounded-full mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300"></div>
-                <p class="opacity-0 text-white text-xl group-hover:opacity-80 transform duration-500 mb-5">
+                <div className="w-16 h-2 bg-red-500 rounded-full mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300"></div>
+                <p className="opacity-0 text-white text-xl group-hover:opacity-80 transform duration-500 mb-5">
                   سامانه ای است برای پیگیری و رصد و تسهیل ارتباطات و مراودات
                   مالی اعضا با یکدیگر.
                 </p>
@@ -83,10 +83,10 @@ export default function Portfolio() {
           </section>
         </section>
       </div>
-      <div className="grid grid-cols-3 gap-10">
-        <section class="container mx-auto transform duration-500">
-          <article class="shadow-lg mx-auto relative max-w-md group cursor-pointer">
-            <div class="overflow-hidden">
+      <div className=" lg:flex  gap-10">
+        <section className=" container mx-auto transform duration-500 flex-1">
+          <article className="shadow-lg mx-auto relative max-w-md group cursor-pointer">
+            <div className="overflow-hidden">
               <Image
                 src={MresalatMini}
                 width={100}
@@ -94,24 +94,24 @@ export default function Portfolio() {
                 className="mx-auto w-full h-auto transform hover:scale-110 duration-200"
               />
             </div>
-            <div class="p-7 my-auto pb-12 ">
-              <h1 class="text-4xl font-semibold text-gray-800 mt-4">
+            <div className="p-7 my-auto pb-12 ">
+              <h1 className="text-4xl font-semibold text-gray-800 mt-4">
                 همیاری اجتماعی
               </h1>
-              <p class="text-2xl text-gray-400 mt-4 leading-relaxed">
+              <p className="text-2xl text-gray-400 mt-4 leading-relaxed">
                 سامانه همیاری اجتماعی سامانه ای است برای متقاضیان عضویت در نهاد
                 مردمی رسالت که با احراز هویت غیرحضوری روند ثبت نام اعضا را تسهیل
                 بخشیده است.
               </p>
             </div>
-            <div class="border-t pt-8 pb-8 text-center text-base text-gray-400 uppercase tracking-widest  group-hover:text-gray-600 bg-gray-50">
+            <div className="border-t pt-8 pb-8 text-center text-base text-gray-400 uppercase tracking-widest  group-hover:text-gray-600 bg-gray-50">
               بیشتر
             </div>
           </article>
         </section>
-        <section class="container mx-auto transform duration-500">
-          <article class="shadow-lg mx-auto relative max-w-md group cursor-pointer">
-            <div class="overflow-hidden">
+        <section className=" lg:flex container mx-auto transform duration-500 flex-1">
+          <article className="shadow-lg mx-auto relative max-w-md group cursor-pointer">
+            <div className="overflow-hidden">
               <Image
                 src={MbazarMini}
                 width={100}
@@ -119,24 +119,24 @@ export default function Portfolio() {
                 className="mx-auto w-full h-auto transform hover:scale-110 duration-200"
               />
             </div>
-            <div class="p-7 my-auto pb-12 ">
-              <h1 class="text-4xl font-semibold text-gray-800 mt-4">
+            <div className="p-7 my-auto pb-12 ">
+              <h1 className="text-4xl font-semibold text-gray-800 mt-4">
                 ام بازار
               </h1>
-              <p class="text-2xl text-gray-400 mt-4 leading-relaxed">
+              <p className="text-2xl text-gray-400 mt-4 leading-relaxed">
                 سامانه ای برای عرضه و تقاضا منصفانه و اعتمادساز برای اعضا و هسته
                 های کارآفرینی اجتماعی و زمینه سازی برای تامین کالا جهت فعالیت
                 های تولیدی و خدماتی هسته های کارآفرینی اجتماعی
               </p>
             </div>
-            <div class="border-t pt-8 pb-8 text-center text-base text-gray-400 uppercase tracking-widest  group-hover:text-gray-600 bg-gray-50">
+            <div className="border-t pt-8 pb-8 text-center text-base text-gray-400 uppercase tracking-widest  group-hover:text-gray-600 bg-gray-50">
               بیشتر
             </div>
           </article>
         </section>
-        <section class="container mx-auto transform duration-500">
-          <article class="shadow-lg mx-auto relative max-w-md group cursor-pointer">
-            <div class="overflow-hidden">
+        <section className="container mx-auto transform duration-500 flex-1">
+          <article className="shadow-lg mx-auto relative max-w-md group cursor-pointer">
+            <div className="overflow-hidden">
               <Image
                 src={MhesamMini}
                 width={100}
@@ -144,14 +144,16 @@ export default function Portfolio() {
                 className="mx-auto w-full h-auto transform hover:scale-110 duration-200"
               />
             </div>
-            <div class="p-7 my-auto pb-12 ">
-              <h1 class="text-4xl font-semibold text-gray-800 mt-4">ام حسام</h1>
-              <p class="text-2xl text-gray-400 mt-4 leading-relaxed">
+            <div className="p-7 my-auto pb-12 ">
+              <h1 className="text-4xl font-semibold text-gray-800 mt-4">
+                ام حسام
+              </h1>
+              <p className="text-2xl text-gray-400 mt-4 leading-relaxed">
                 سامانه ای است برای پیگیری و رصد و تسهیل ارتباطات و مراودات مالی
                 اعضا با یکدیگر.
               </p>
             </div>
-            <div class="border-t pt-8 pb-8 text-center text-base text-gray-400 uppercase tracking-widest  group-hover:text-gray-600 bg-gray-50">
+            <div className="border-t pt-8 pb-8 text-center text-base text-gray-400 uppercase tracking-widest  group-hover:text-gray-600 bg-gray-50">
               بیشتر
             </div>
           </article>
